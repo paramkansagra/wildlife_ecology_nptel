0c384a42fc79fbba9f67c67bd95ddd8e53882551
Param Kansagra <paramjayesh.kansagra2021@vitstudent.ac.in>
2024-08-31 01:37:13 +0530

    Added speed insights for vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AppTitle from "@/components/landing-page";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
 	return (
 		<>
 			<AppTitle />
+			<SpeedInsights />
 		</>
 	);
 }
